9a9e0bd65991c7224631220404b4a10ba37c5023
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2018-03-24 02:53:01 +0100

    Started implementing password char workaround

--- MarkdownToRW/MarkdownToRWGUI/MainWindow.xaml.cs
+++ MarkdownToRW/MarkdownToRWGUI/MainWindow.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.IO;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media.Imaging;
+using DragonMarkdown.Utility;
 using MarkdownToRWGUI.Models;
 
 namespace MarkdownToRWGUI
 {
     public class MainWindow : Window
     {
+        public TextBox TxtPassword;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,12 +24,27 @@ namespace MarkdownToRWGUI
                 Bitmap b = new Bitmap("rw-logo_250.ico");
                 Icon = new WindowIcon(b);
             }
-
+            
+            Closing += OnClosing;
+            
             Settings set = SettingsManager.LoadSettings();
 
-            TextBox txtPassword = this.FindControl<TextBox>("txtPassword");
+            TxtPassword  = this.FindControl<TextBox>("txtPassword");
+            TxtPassword.PropertyChanged += TxtPasswordOnPropertyChanged;
+            
+            
+            DataContext = new MainWindowViewModel { ThisWindow = this, TxtPassword = TxtPassword, Status = "Open a markdown file to start!", AllowInput = true, RememberCredentials = set.ShouldLoadCredentials, Username = set.Username, Password = set.Password};
+        }
 
-            DataContext = new MainWindowViewModel { ThisWindow = this, TxtPassword = txtPassword, Status = "Open a markdown file to start!", AllowInput = true, RememberCredentials = set.ShouldLoadCredentials, Username = set.Username, Password = set.Password};
+        private void TxtPasswordOnPropertyChanged(object sender, AvaloniaPropertyChangedEventArgs avaloniaPropertyChangedEventArgs)
+        {
+            Console.WriteLine(avaloniaPropertyChangedEventArgs.NewValue.ToString());
+            TxtPassword.Text = DragonUtil.GetPasswordChars(TxtPassword.Text.Length, 'x');
+        }
+
+        private void OnClosing(object sender, CancelEventArgs cancelEventArgs)
+        {
+            
         }
 
         private void InitializeComponent()
