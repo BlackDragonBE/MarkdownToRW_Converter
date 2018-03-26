855475ddc27fe8ef31655503e7a22a9cabffda82
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2018-03-26 22:29:47 +0200

    Changed version

--- MarkdownToRW/MarkdownConverter/DragonVersion.cs
+++ MarkdownToRW/MarkdownConverter/DragonVersion.cs
@@ -2,6 +2,6 @@ namespace DragonMarkdown
 {
     public static class DragonVersion
     {
-        public static readonly decimal VERSION = 1.05m;
+        public static readonly decimal VERSION = 1.11m;
     }
 }
